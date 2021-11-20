d9b8cfe4afa57148fd254edf4f3c3985336b163b
dfoulks <dfoulks@apache.org>
2021-11-19 22:00:07 -0600

    simon work

--- simon.py
+++ simon.py
@@ -1,21 +1,34 @@
-#!/usr/bin/python3
 import gpiozero
 import threading
 import time
 import random
-import subprocess
+import os
+from subprocess import call
 
-# Red, Green, Blue, Yellow
-LIGHTS = [5, 6, 13, 19]
-NOTES = [ "E3", "A4", "E4", "C#4"]
+# green, red, blue, yellow
+LIGHTS = [33, 37, 35, 31]
+BUTTONS = [11, 15, 13, 7]
+NOTES = ["E3", "A4", "E4", "Cs4"]
 
 simon = {
-  "buzzer": gpiozero.TonalBuzzer(23),
-  "blue": {
-    "button": gpiozero.Button(26),
-    "note": "E3",
-    "light_pin": False,
+    "blue": {
+        "button": gpiozero.Button(12),
+        "led": gpiozero.LED(6)
+        }
+    "red": {
+        "button": gpiozero.Button(16),
+        "led": gpiozero.LED(13)
+        }
+    "green": {
+        "button": gpiozero.Button(20),
+        "led": gpiozero.LED(19)
+        }
+    "yellow": {
+        "button": gpiozero.Button(21),
+        "led": gpiozero.LED(26)
+        }
     }
-}
 
-print(simon["blue"]["button"])
+simon["blue"]["button"].when_pressed = simon["blue"]["led"].on()
+simon["blue"]["button"].when_released = simon["blue"]["led"].off()
+        
