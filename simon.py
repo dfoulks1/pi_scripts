063dde466fd954d62fda08c9eb53c7480c927d4c
dfoulks <dfoulks@apache.org>
2021-11-20 16:53:42 -0600

    fetch color by button comparison

--- simon.py
+++ simon.py
@@ -43,9 +43,12 @@ simon = {
         },
     }
 
-def verify_player_selection(color):
+def verify_player_selection(button):
     global current_step_of_level, current_level, is_won_cur_lvl, is_game_over
-    print(color)
+    for color in simon.keys():
+        if button is simon[color]["button"]:
+            color = simon[color]
+
     if not displaying and not is_won_cur_lvl and not is_game_over:
         color["led"].on()
         buzzer.play(color["tone"])
